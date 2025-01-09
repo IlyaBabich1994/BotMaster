f8b24948791be7c394da8f3db90d234d3699a19e
IkitTheClaw <IkitTheClaw@gmail.com>
2025-01-09 23:04:58 +0300

    make an a status-check endpoint v2

--- src/main/java/com/example/demo/controlers/HealthController.java
+++ src/main/java/com/example/demo/controlers/HealthController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controlers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,21 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/api/v1/health")
-    public String health() {
-        return "<!DOCTYPE html>" +
-                "<html lang='ru'>" +
-                "<head>" +
-                "<meta charset='UTF-8'>" +
-                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
-                "<title>Сообщение</title>" +
-                "<style>" +
-                "body { display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif; background-color: #f0f0f0; }" +
-                "h1 { font-size: 24px; color: #333; }" +
-                "</style>" +
-                "</head>" +
-                "<body>" +
-                "<h1>Ты думал тут что-то будет?</h1>" +
-                "</body>" +
-                "</html>";
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
     }
 }
